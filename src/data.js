79e55b8107a2b0e1858c27c845bde04359f79677
NikhilAnkola <nikhilankola2@gmail.com>
2025-08-07 11:53:04 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,18 +3,21 @@ const products = [
     id: 1,
     name: "Wireless Headphones",
     price: "5000",
+    image: "./images/headphone.png",
     description: "High-quality wireless headphones with noise cancellation."
   },
   {
     id: 2,
     name: "Smartwatch",
     price: "2000",
+    image: "./images/smartwatch.png",
     description: "Feature-packed smartwatch with heart rate and GPS tracking."
   },
   {
     id: 3,
     name: "Bluetooth Speaker",
     price: "10000",
+    image: "./images/bluetoothspeaker.png",
     description: "Portable Bluetooth speaker with deep bass and long battery life."
   }
 ];
