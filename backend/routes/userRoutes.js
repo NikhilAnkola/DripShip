cebf92bd7b13ada4a5323fe0fff143a42dc285a7
NikhilAnkola <nikhilankola2@gmail.com>
2025-10-12 21:49:55 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/User.js');
 
 // GET /api/users -> fetch all users (for testing purposes)
 router.get('/', async (req, res) => {
